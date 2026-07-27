9fac415ad3c8f3ac20c69465cffa50df1b986e90
Derek Baert <81194166+DerekBaert@users.noreply.github.com>
2026-07-27 16:05:49 -0500

    Added 'normal' movement controls, currently no rotation

--- Gyms/Derek/character_controller.gd
+++ Gyms/Derek/character_controller.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
-@export var forward_speed = 3.0
-@export var backward_speed = 1.0
-@export var turn_speed = 0.8
+@export var forward_speed: float = 300.0
+@export var backward_speed: float = 100.0
+@export var turn_speed: float = 1.0
+@export var tank_controls: bool = false
+@onready var camera = $"../Camera3D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,17 +17,30 @@ func _process(delta: float) -> void:
 	pass
 	
 func get_input(delta):
-	var vy = velocity.y
-	velocity = Vector3.ZERO
-	var move = Input.get_axis("back", "forward")
-	var turn = Input.get_axis("right", "left")
-	if move < 0:
-		velocity += -transform.basis.z * move * backward_speed
+	if tank_controls:		
+		var vy: float = velocity.y
+		velocity = Vector3.ZERO
+		var move: float = Input.get_axis("back", "forward")
+		var turn: float = Input.get_axis("right", "left")
+		if move < 0:
+			velocity += -transform.basis.z * move * backward_speed  * delta
+			pass
+		elif move > 0:
+			velocity += -transform.basis.z * move * forward_speed * delta
+			pass
+		rotate_y(turn_speed * turn * delta)
+		velocity.y = vy
 		pass
-	elif move > 0:
-		velocity += -transform.basis.z * move * forward_speed
+	else:
+		camera.get_global_transform().basis.z 
+		var vy: float = velocity.y
+		velocity = Vector3.ZERO
+		var raw_input := Input.get_vector("left", "right", "forward", "back")
+		var forward = camera.global_basis.z
+		var right = camera.global_basis.x		
+		var move_direction = forward * raw_input.y + right * raw_input.x		
+		move_direction.y = 0.0
+		move_direction = move_direction.normalized()
+		velocity = move_direction * forward_speed * delta
 		pass
-	# velocity += -transform.basis.z * move * speed
-	rotate_y(turn_speed * turn * delta)
-	velocity.y = vy
 	pass
