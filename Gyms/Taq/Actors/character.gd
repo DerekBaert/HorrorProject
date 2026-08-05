36ad240ce807023044774fb92d7cb1b3ac97ed06
Q Yoneda <57389274+qyoneda@users.noreply.github.com>
2026-08-05 17:44:51 -0500

    Prototyping Dialogue and UI system

--- Gyms/Taq/Actors/character.gd
+++ Gyms/Taq/Actors/character.gd
@@ -1,5 +1,15 @@
 extends CharacterBody2D
 
+@export var speed:float = 500
+
 func _physics_process(delta: float) -> void:
 	position.y += 300 * delta
+	walk(delta)
 	move_and_slide()
+
+
+func walk(delta)-> void:
+	var direction = Input.get_axis("left","right")
+	velocity = Vector2(direction * speed, position.y)
+	
+	
---
+++ Gyms/Taq/Scenes/ItemUITrigger.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+@onready var area2d: Area2D = $Area2D
+
+@export var dialogueBalloon:PackedScene
+@export var dialogueResource:DialogueResource
+@export var dialogueTitle:String
+
+func _ready() -> void:
+	area2d.body_entered.connect(_onBodyEntered)
+	area2d.body_exited.connect(_onBodyExited)
+
+func _onBodyEntered(body)->void:
+	if body.is_in_group("Player"):
+		DialogueManager.show_dialogue_balloon_scene(
+			dialogueBalloon.resource_path, load(dialogueResource.resource_path), dialogueTitle
+			)
+			
+func _onBodyExited(body)->void:
+	print("exited")
+	if body.is_in_group("Player"):
+		if get_parent().get_node("ItemUI"):
+			get_parent().get_node("ItemUI").queue_free()
