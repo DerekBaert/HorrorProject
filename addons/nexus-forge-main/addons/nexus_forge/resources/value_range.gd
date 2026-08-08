cdab70b73017537c6e326d4cda369d440cc27f88
Winson <winsonlosesome@gmail.com>
2026-08-08 10:00:49 -0500

    Deleted Nexus Forge on my end

--- addons/nexus-forge-main/addons/nexus_forge/resources/value_range.gd
+++
@@ -1,8 +0,0 @@
-class_name ValueRange
-extends Resource
-## Base class of [RangeFloat] and [RangeInt].
-
-
-## Returns the type of range that this resource holds.
-func range_type() -> int:
-	return TYPE_NIL
