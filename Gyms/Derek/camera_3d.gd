fc68b39b44b382149f7187b5e10d1f4675f7ca49
Derek Baert <81194166+DerekBaert@users.noreply.github.com>
2026-08-16 12:56:19 -0500

    Adjusting points on camera track in hallway, doesn't have it's little freak out but can be smoother

--- Gyms/Derek/camera_3d.gd
+++ Gyms/Derek/camera_3d.gd
@@ -9,5 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# var new_rot = lerp(transform.basis, transform.basis.looking_at(target.position), 1)
+	# transform.basis.looking_at(target.position)
 	look_at(target.position)
 	pass
