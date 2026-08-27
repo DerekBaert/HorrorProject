8a270207afe755de630862a0d71d218b9d314c23
Derek Baert <81194166+DerekBaert@users.noreply.github.com>
2026-08-26 21:09:32 -0500

    Switched to animation tree for animation blending

--- Gyms/Derek/character_controller.gd
+++ Gyms/Derek/character_controller.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 signal characterMovementChanged(is_moving:bool)
 
-@export var forward_speed: float = 150.0
+@export var forward_speed: float = 50.0
 @export var backward_speed: float = forward_speed * 0.5
 @export var turn_speed: float = 1.0
 @export var auto_turn_speed: float = 5.0
--- Gyms/Wins/characters/player_animations.gd
+++ Gyms/Wins/characters/player_animations.gd
@@ -1,20 +1,33 @@
 extends Node3D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+enum {IDLE,WALK}
+var current_anim = IDLE
+@onready var anim_tree: AnimationTree = $AnimationTree
 
-func _ready() -> void:
-	animation_player.current_animation = "Anim_AndrewIdle/Animation"
+func _process(delta: float) -> void:
+	_handle_animations(delta)
+	pass
+
+func _handle_animations(delta):
+	match current_anim:
+		IDLE:
+			anim_tree.set("parameters/Movement/transition_request","Idle")
+			#walk_val = lerp(walk_val,0.0,blend_speed * delta)
+		WALK:
+			#walk_val = lerp(walk_val,1.0,blend_speed * delta)
+			anim_tree.set("parameters/Movement/transition_request","Walk")
 	pass
 
 func _set_animation(animation):
-	animation_player.current_animation = animation
+	current_anim = animation
 	pass
-
-
+	
 func _on_character_body_3d_character_movement_changed(is_moving: bool) -> void:
 	if(is_moving):
-		animation_player.current_animation = "Anim_AndrewWalk/Animation"
+		#animation_player.current_animation = "Anim_AndrewWalk/Animation"
+		current_anim = WALK
 	else:
-		animation_player.current_animation = "Anim_AndrewIdle/Animation"
+		#animation_player.current_animation = "Anim_AndrewIdle/Animation"
+		current_anim = IDLE
 		pass
 	pass # Replace with function body.
